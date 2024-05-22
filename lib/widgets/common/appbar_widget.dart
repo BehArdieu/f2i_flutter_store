c800757d6fd076a13c4808ab182bf7c841da204a
Comlan ALOTIN <alotinbehardieu@gmail.com>
2024-05-22 10:08:05 +0200

    Ajout d'une police au niveau de l'Appbar

--- lib/widgets/common/appbar_widget.dart
+++ lib/widgets/common/appbar_widget.dart
@@ -6,11 +6,18 @@ class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text('Store'),
+      title: const Text(
+        'Store',
+        style: TextStyle(
+          color: Color.fromRGBO(255, 0, 255, 1),
+          fontSize: 20,
+          fontFamily: 'SpaceGrotesk'
+        ),
+      ),
       centerTitle: true,
     );
   }
-  
+
   @override
   // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(55);
